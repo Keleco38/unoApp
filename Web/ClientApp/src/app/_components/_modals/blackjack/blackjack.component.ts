3ef6c2eb55164114da78fd84814a6e561eba51e7
tonymarkoc <tonymarkoc@gmail.com>
2019-09-03 00:59:34 +0200

    Added lowercase blackjack component

---
+++ Web/ClientApp/src/app/_components/_modals/blackjack/blackjack.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-blackjack',
+  templateUrl: './blackjack.component.html',
+  styleUrls: ['./blackjack.component.css']
+})
+export class BlackjackComponent implements OnInit {
+  blackjackNumber: number = 0;
+  numbersHit: number[] = [];
+  lastNumberHit: number = 0;
+  possibleNumbers: number[] = [];
+  constructor(private _activeModal: NgbActiveModal) {}
+
+  ngOnInit() {
+    for (let i = 0; i < 4; i++) {
+      this.possibleNumbers.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10);
+    }
+  }
+
+  hitMe() {
+    this.lastNumberHit = this.possibleNumbers[Math.floor(Math.random() * this.possibleNumbers.length)];
+    this.numbersHit.push(this.lastNumberHit);
+    this.blackjackNumber += this.lastNumberHit;
+    if (this.blackjackNumber >= 21) {
+      this.confirm();
+    }
+  }
+
+  confirm() {
+    this._activeModal.close(this.blackjackNumber);
+  }
+}
