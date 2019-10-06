e80dcaff910b05895781e478b4a6857270b5f17c
tonymarkoc <tonymarkoc@gmail.com>
2019-10-06 11:41:56 +0200

    css tweaks for showing cards

--- Web/ClientApp/src/app/_components/_modals/show-cards/show-cards.component.ts
+++ Web/ClientApp/src/app/_components/_modals/show-cards/show-cards.component.ts
@@ -4,6 +4,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Component, OnInit, Input } from '@angular/core';
 import { Card } from 'src/app/_models/card';
 import { CardValue } from 'src/app/_models/enums';
+import { UserSettings } from 'src/app/_models/userSettings';
 
 @Component({
   selector: 'app-show-cards',
@@ -14,10 +15,12 @@ export class ShowCardsComponent implements OnInit {
   @Input() cardsAndNames: KeyValue<string, Card[]>[];
   @Input() detailed: boolean;
   private allCards: any;
+  userSettings: UserSettings;
   constructor(private _activeModal: NgbActiveModal, private _utilityService:UtilityService) {}
 
   ngOnInit() {
     this.allCards = this._utilityService.allCards;
+    this.userSettings=this._utilityService.userSettings;
   }
 
   closeModal() {
