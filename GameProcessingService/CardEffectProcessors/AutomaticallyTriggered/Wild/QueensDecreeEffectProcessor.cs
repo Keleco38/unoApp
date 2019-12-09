88bc90a12c9059f0d0c9f2101b9e995194417b30
tonymarkoc <tonymarkoc@gmail.com>
2019-12-09 01:11:44 +0100

    fixed queens decree

--- GameProcessingService/CardEffectProcessors/AutomaticallyTriggered/Wild/QueensDecreeEffectProcessor.cs
+++ GameProcessingService/CardEffectProcessors/AutomaticallyTriggered/Wild/QueensDecreeEffectProcessor.cs
@@ -24,40 +24,53 @@ namespace GameProcessingService.CardEffectProcessors.AutomaticallyTriggered.Wild
             {
                 if (player.Cards.Count <= 5 && player.Cards.FirstOrDefault(x => x.Value == CardValue.QueensDecree) != null)
                 {
-                    messageToLog += $"{player.User.Name} had less than 5 or less cards and queen's decree, auto effect is activated. ";
+                    messageToLog += $"{player.User.Name} had 5 or less cards and queen's decree, auto effect is activated. ";
                     if (game.Players.Count == 2)
                     {
                         var nextPlayer = _gameManager.GetNextPlayer(game, player, game.Players, false);
-                        messageToLog += $"Next player ({nextPlayer.User.Name}) will draw a card. ";
 
-
-                        var kingsDecreeResult = BlockedByKingsDecree(messageToLog, nextPlayer, game);
-                        messageToLog = kingsDecreeResult.MessageToLog;
-                        if (!kingsDecreeResult.ActivatedKingsDecree)
+                        if (nextPlayer.Cards.Count < player.Cards.Count)
                         {
-                            _gameManager.DrawCard(game, nextPlayer, 1, false);
+                            messageToLog += $"Next player ({nextPlayer.User.Name}) will draw a card. ";
+                            var kingsDecreeResult = BlockedByKingsDecree(messageToLog, nextPlayer, game);
+                            messageToLog = kingsDecreeResult.MessageToLog;
+                            if (!kingsDecreeResult.ActivatedKingsDecree)
+                            {
+                                _gameManager.DrawCard(game, nextPlayer, 1, false);
+                            }
                         }
+
+
                     }
                     else
                     {
                         var previousPlayer = _gameManager.GetNextPlayer(game, player, game.Players, false);
                         var nextPlayer = _gameManager.GetNextPlayer(game, player, game.Players, false);
 
-                        messageToLog += $"Player to the left and right ({previousPlayer.User.Name} and {nextPlayer.User.Name}) will draw a card. ";
 
-                        var kingsDecreeResult = BlockedByKingsDecree(messageToLog, nextPlayer, game);
-                        messageToLog = kingsDecreeResult.MessageToLog;
-                        if (!kingsDecreeResult.ActivatedKingsDecree)
+                        if (nextPlayer.Cards.Count < player.Cards.Count)
                         {
-                            _gameManager.DrawCard(game, nextPlayer, 1, false);
+                            messageToLog += $"Next player ({nextPlayer.User.Name}) will draw a card. ";
+                            var kingsDecreeResult = BlockedByKingsDecree(messageToLog, nextPlayer, game);
+                            messageToLog = kingsDecreeResult.MessageToLog;
+                            if (!kingsDecreeResult.ActivatedKingsDecree)
+                            {
+                                _gameManager.DrawCard(game, nextPlayer, 1, false);
+                            }
                         }
 
-                        kingsDecreeResult = BlockedByKingsDecree(messageToLog, previousPlayer, game);
-                        messageToLog = kingsDecreeResult.MessageToLog;
-                        if (!kingsDecreeResult.ActivatedKingsDecree)
+                        if (previousPlayer.Cards.Count < player.Cards.Count)
                         {
-                            _gameManager.DrawCard(game, previousPlayer, 1, false);
+                            messageToLog += $"Previous player player ({previousPlayer.User.Name}) will draw a card. ";
+                            var kingsDecreeResult = BlockedByKingsDecree(messageToLog, previousPlayer, game);
+                            messageToLog = kingsDecreeResult.MessageToLog;
+                            if (!kingsDecreeResult.ActivatedKingsDecree)
+                            {
+                                _gameManager.DrawCard(game, previousPlayer, 1, false);
+                            }
                         }
+
+
 
 
                     }
