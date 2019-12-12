00f92efcea6fe82830a7e5cbfd1103f6297362c8
tonymarkoc <tonymarkoc@gmail.com>
2019-12-12 22:07:28 +0100

    updated change log

--- EntityObjects/Cards/Wild/Greed.cs
+++ EntityObjects/Cards/Wild/Greed.cs
@@ -19,6 +19,6 @@ namespace EntityObjects.Cards.Wild
         public string ImageUrl { get; }
         public bool RequirePickColor => true;
         public bool RequireTargetPlayer => false;
-        public string Description => "Select a player, for the next 3 turns they are forced to draw 1 card from deck. Multiple greed can stack. Double draw does not affect greed.";
+        public string Description => "Select a player, for the next 3 turns they are forced to draw 1 card from deck. Multiple greed can stack. Can be affected by double draw and king's decree.";
     }
 }
